91c526f631ec2f4ce3c98cbff53b4b8a184fb34f
GravermanDev <swiatmetalu@gmail.com>
2024-04-14 19:01:25 +0200

    multiple pongs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::net::{TcpStream, TcpListener};
-use std::io::{Write};
+use std::io::{Write, Read};
 use anyhow::Error;
 
+const PING_COMMAND: [u8; 14] = [0x2a, 0x31, 0xd, 0xa, 0x24, 0x34, 0xd, 0xa, 0x70, 0x69, 0x6e, 0x67, 0xd, 0xa];
+
 fn handle_client(stream: &mut TcpStream) -> Result<(), Error> {
-    stream.write_all("+PONG\r\n".as_bytes())?;
-    Ok(())
+    loop {
+        let mut buf = [0u8; 128];
+        let n = stream.read(&mut buf)?;
+
+        if !buf[..n].starts_with(&PING_COMMAND) {
+            return Ok(());
+        }
+        stream.write_all("+PONG\r\n".as_bytes())?;
+    }
 }
 
 fn main() {
@@ -15,7 +24,10 @@ fn main() {
     for stream in listener.incoming() {
          match stream {
              Ok(mut stream) => {
-                 handle_client(&mut stream);
+                 match handle_client(&mut stream) {
+                     Ok(_) => (),
+                     Err(error) => eprintln!("Error handling connection: {}", error),
+                 }
              }
              Err(e) => {
                  println!("error: {}", e);
